4be8de4239527a3f6ce84e88b0ddc330a177b7e7
Devaev Maxim <mdevaev@gmail.com>
2012-11-21 02:18:32 +0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup
+
+
+setup(
+	name="rtfetch",
+	version="0.1",
+	url="https://github.com/mdevaev/rtfetch",
+	license="GPLv3",
+	author="Devaev Maxim",
+	author_email="mdevaev@gmail.com",
+	description="rtfetch -- Update rtorrent files from popular trackers",
+	platforms="any",
+	packages=[
+		"rtlib",
+		"rtlib/fetchers",
+	],
+	scripts=[
+		"rtfetch.py",
+	]
+	classifiers=[
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Development Status :: 3 - Alpha",
+		"Programming Language :: Python",
+		"Operating System :: OS Independent",
+	],
+)
+
