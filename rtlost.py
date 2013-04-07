2fe12974f4951c2dca4da814e12167d7e4d670db
Devaev Maxim <mdevaev@gmail.com>
2013-04-07 19:43:53 +0400

    rtlost: refactoring

--- rtlost.py
+++ rtlost.py
@@ -28,55 +28,60 @@ import argparse
 
 
 ##### Public methods #####
-def torrentFiles(torrent, prefix = "") :
-	# XXX: See https://wiki.theory.org/BitTorrentSpecification for details
-	bencode_dict = torrent.bencode()
-	base = os.path.join(prefix, bencode_dict["info"]["name"])
-	if not bencode_dict["info"].has_key("files") : # Single File Mode
-		return [base]
-	else : # Multiple File Mode
-		files_list = [base]
-		for file_dict in bencode_dict["info"]["files"] :
-			for (index, _) in enumerate(file_dict["path"]) :
-				files_list.append(os.path.join(prefix, base, os.path.sep.join(file_dict["path"][0:index + 1])))
-		return files_list
-
-def torrentsProvides(torrents_dir_path, data_dir_path) :
+def torrentsProvides(src_dir_path, data_dir_path) :
 	data_files_list = []
-	for torrent in tfile.torrents(torrents_dir_path).values() :
-		for file_path in torrentFiles(torrent, data_dir_path) :
+	for torrent in tfile.torrents(src_dir_path).values() :
+		for file_path in torrent.files(data_dir_path) :
 			data_files_list.append(file_path)
 	return data_files_list
 
-def onError(exception) :
+def onWalkError(exception) :
 	raise exception
 
 def dataProvides(data_dir_path) :
 	data_files_list = []
-	for (root, dirs_list, files_list) in os.walk(data_dir_path, onerror=onError) :
+	for (root, dirs_list, files_list) in os.walk(data_dir_path, onerror=onWalkError) :
 		for item in dirs_list + files_list :
 			data_files_list.append(os.path.join(root, item))
 	return data_files_list
 
-def searchLost(torrents_dir_path, data_dir_path) :
+def searchLost(src_dir_path, data_dir_path) :
 	data_dir_path = os.path.abspath(data_dir_path)
-	torrents_tree_set = set(torrentsProvides(torrents_dir_path, data_dir_path))
+	torrents_tree_set = set(torrentsProvides(src_dir_path, data_dir_path))
 	data_tree_set = set(dataProvides(data_dir_path))
 	for file_path in sorted(data_tree_set.difference(torrents_tree_set)) :
 		print file_path
 
 
+###
+def requiredOptions(main_action, options, actions_list) :
+	errors_list = []
+	for action in actions_list :
+		if getattr(options, action.dest) is None :
+			errors_list.append("/".join(action.option_strings))
+	if len(errors_list) != 0 :
+		print "%s: these arguments are required for %s" % (os.path.basename(sys.argv[0]), "/".join(main_action.option_strings))
+		for error in errors_list :
+			print "\t" + error
+		sys.exit(1)
+
+
 ##### Main #####
 def main() :
 	cli_parser = argparse.ArgumentParser(description="Shows a list of files in the specified directory, not registered in rtorrent")
-	cli_parser.add_argument("-t", "--torrents-dir", dest="torrents_dir_path", action="store", required=True, metavar="<dir>")
-	cli_parser.add_argument("-d", "--data-dir",     dest="data_dir_path",     action="store", required=True, metavar="<dir>")
-	cli_options = cli_parser.parse_args(sys.argv[1:])
+	modes_group = cli_parser.add_mutually_exclusive_group(required=True)
+	lost_files_opt =    modes_group.add_argument("--lost-files",    dest="lost_files_flag",    action="store_true", default=False)
 
-	searchLost(
-		cli_options.torrents_dir_path,
-		cli_options.data_dir_path,
-	)
+	src_dir_opt =  cli_parser.add_argument("-s", "--source-dir", dest="src_dir_path",  action="store", metavar="<dir>")
+	data_dir_opt = cli_parser.add_argument("-d", "--data-dir",   dest="data_dir_path", action="store", metavar="<dir>")
+
+	cli_options = cli_parser.parse_args(sys.argv[1:])
+	if cli_options.lost_files_flag :
+		requiredOptions(lost_files_opt, cli_options, (src_dir_opt, data_dir_opt))
+		searchLost(
+			cli_options.src_dir_path,
+			cli_options.data_dir_path,
+		)
 
 
 ###
