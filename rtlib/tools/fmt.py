da7a6e19a6eaa272c29b5ef2f9219e33421fa855
Devaev Maxim <mdevaev@gmail.com>
2013-05-10 00:13:38 +0400

    tools.fmt: data formatters

---
+++ rtlib/tools/fmt.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+##### Public methods #####
+def formatSize(size) :
+	if size >= 1073741824 :
+		return "%.1f Gb" % (size / 1073741824.0)
+	elif size > 1048576 :
+		return "%.1f Mb" % (size / 1048576.0)
+	elif size > 1024 :
+		return "%.1f Kb" % (size / 1024.0)
+	else :
+		return "%d bytes" % (size)
+
