ee88a74e842887a2a1779954bcc50ceea129dfe1
Devaev Maxim <mdevaev@gmail.com>
2013-05-14 14:03:31 +0400

    tools.fs.treeListToDict(): convert plain files list to dict

--- rtlib/tools/fs.py
+++ rtlib/tools/fs.py
@@ -15,3 +15,15 @@ def treeList(dir_path, on_walk_error = onWalkError) :
 			tree_files_list.append(os.path.join(root, item))
 	return tree_files_list
 
+def treeListToDict(files_list) :
+	tree_dict = {}
+	for path in files_list :
+		parts_list = os.path.normpath(path).split(os.path.sep)
+		local_dict = tree_dict
+		for (index, part) in enumerate(parts_list) :
+			if index != 0 and len(part) == 0 :
+				continue
+			local_dict.setdefault(part, {})
+			local_dict = local_dict[part]
+	return tree_dict
+
