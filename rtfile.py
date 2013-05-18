309839bc2b349e2af9d72c398c394e21511f1b23
Devaev Maxim <mdevaev@gmail.com>
2013-05-18 21:35:56 +0400

    rtfile: print extra fields

--- rtfile.py
+++ rtfile.py
@@ -22,6 +22,7 @@
 
 from rtlib import tfile
 from rtlib import clients
+from rtlib import clientlib
 
 from rtlib import tools
 import rtlib.tools.fmt # pylint: disable=W0611
@@ -30,28 +31,64 @@ import rtlib.tools.fs
 import sys
 import socket
 import operator
+import itertools
+import datetime
 import argparse
 
 
 ##### Public methods #####
-def printFilesDict(files_dict, depth = 0, prefix="\t") :
+def makeFilesList(files_dict, depth = 0, prefix = "\t") :
+	text = ""
 	for (key, value_dict) in sorted(files_dict.iteritems(), key=operator.itemgetter(0)) :
-		print prefix + "*   " * depth + key
-		printFilesDict(value_dict, depth + 1)
+		text += prefix + "*   " * depth + key + "\n"
+		text += makeFilesList(value_dict, depth + 1)
+	return text
 
-def printPrettyMeta(torrent, client, hashes_list) :
-	print "Torrent:     ", torrent.path()
-	print "Name:        ", torrent.name()
-	print "Hash:        ", torrent.hash()
-	print "Comment:     ", torrent.comment()
-	print "Size:        ", tools.fmt.formatSize(torrent.size())
+
+###
+def formatSize(torrent) :
+	return tools.fmt.formatSize(torrent.size())
+
+def formatAnnounce(torrent) :
+	return ( torrent.announce() or "" )
+
+def formatAnnounceListPretty(torrent) :
+	return " ".join(itertools.chain.from_iterable(torrent.announceList()))
+
+def formatCreationDatePretty(torrent) :
+	return ( datetime.datetime.utcfromtimestamp(torrent.creationDate()) if not torrent.creationDate() is None else "" )
+
+def formatCreatedBy(torrent) :
+	return ( torrent.createdBy() or "" )
+
+def formatComment(torrent) :
+	return ( torrent.comment() or "" )
+
+def formatClientPath(torrent, client) :
+	try :
+		return client.dataPrefix(torrent)
+	except clientlib.NoSuchTorrentError :
+		return ""
+
+def formatFilesList(torrent) :
+	return makeFilesList(tools.fs.treeListToDict(torrent.files()))
+
+def printPrettyMeta(torrent, client) :
+	print "Torrent:       ", torrent.path()
+	print "Name:          ", torrent.name()
+	print "Hash:          ", torrent.hash()
+	print "Size:          ", formatSize(torrent)
+	print "Announce:      ", formatAnnounce(torrent)
+	print "Announce list: ", formatAnnounceListPretty(torrent)
+	print "Creation date: ", formatCreationDatePretty(torrent)
+	print "Created by:    ", formatCreatedBy(torrent)
+	print "Comment:       ", formatComment(torrent)
 	if not client is None :
-		print "Client path: ", ( client.dataPrefix(torrent) if torrent.hash() in hashes_list else "" )
+		print "Client path:   ", formatClientPath(torrent, client)
 	if torrent.isSingleFile() :
-		print "Provides:    ", tuple(torrent.files())[0]
+		print "Provides:      ", tuple(torrent.files())[0]
 	else :
-		print "Provides:"
-		printFilesDict(tools.fs.treeListToDict(torrent.files()))
+		print "Provides:\n%s" % (formatFilesList(torrent))
 
 
 ##### Main #####
@@ -62,6 +99,11 @@ def main() :
 	cli_parser.add_argument(      "--comment",     dest="print_comment_flag",     action="store_true", default=False)
 	cli_parser.add_argument(      "--size",        dest="print_size_flag",        action="store_true", default=False)
 	cli_parser.add_argument(      "--size-pretty", dest="print_size_pretty_flag", action="store_true", default=False)
+	cli_parser.add_argument(      "--announce",             dest="print_announce_flag",             action="store_true", default=False)
+	cli_parser.add_argument(      "--announce-list",        dest="print_announce_list_flag",        action="store_true", default=False)
+	cli_parser.add_argument(      "--creation-date",        dest="print_creation_date_flag",        action="store_true", default=False)
+	cli_parser.add_argument(      "--creation-date-pretty", dest="print_creation_date_pretty_flag", action="store_true", default=False)
+	cli_parser.add_argument(      "--created-by",           dest="print_created_by_flag",           action="store_true", default=False)
 	cli_parser.add_argument(      "--provides",    dest="print_provides_flag",    action="store_true", default=False)
 	cli_parser.add_argument(      "--client-path", dest="print_client_path_flag", action="store_true", default=False)
 	cli_parser.add_argument(      "--make-magnet", dest="print_magnet_flag",      action="store_true", default=False)
@@ -76,33 +118,42 @@ def main() :
 
 	torrents_list = [ tfile.Torrent(item) for item in cli_options.torrents_list ]
 
-	client = hashes_list = None
+	client = None
 	if not cli_options.client_name is None :
 		client_class = clients.CLIENTS_MAP[cli_options.client_name]
 		client = client_class(cli_options.client_url)
-		hashes_list = client.hashes()
 
 	for torrent in torrents_list :
 		if cli_options.print_name_flag :
 			print torrent.name()
 		elif cli_options.print_hash_flag :
 			print torrent.hash()
-		elif cli_options.print_comment_flag :
-			print torrent.comment()
 		elif cli_options.print_size_flag :
 			print torrent.size()
 		elif cli_options.print_size_pretty_flag :
-			print tools.fmt.formatSize(torrent.size())
+			print formatSize(torrent)
+		elif cli_options.print_announce_flag :
+			print formatAnnounce(torrent)
+		elif cli_options.print_announce_list_flag :
+			print formatAnnounceListPretty(torrent)
+		elif cli_options.print_creation_date_flag :
+			print ( torrent.creationDate() or "" )
+		elif cli_options.print_creation_date_pretty_flag :
+			print formatCreationDatePretty(torrent)
+		elif cli_options.print_created_by_flag :
+			print formatCreatedBy(torrent)
+		elif cli_options.print_comment_flag :
+			print formatComment(torrent)
 		elif cli_options.print_provides_flag :
 			for file_path in sorted(torrent.files()) :
 				print file_path
 		elif cli_options.print_client_path_flag :
 			assert not client is None, "Required client"
-			print ( client.dataPrefix(torrent) if torrent.hash() in hashes_list else "" )
+			print formatClientPath(torrent, client)
 		elif cli_options.print_magnet_flag :
 			print torrent.magnet(cli_options.magnet_fields_list)
 		else :
-			printPrettyMeta(torrent, client, hashes_list)
+			printPrettyMeta(torrent, client)
 			if len(torrents_list) > 1 :
 				print
 
