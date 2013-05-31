45d12c37533a19259c626514784360a9ae977029
Devaev Maxim <mdevaev@gmail.com>
2013-05-31 16:57:23 +0400

    rthook-manage-trackers: The tool for managing trackers in rtorrent

--- rthook-disable-retrackers.py
+++ rthook-disable-retrackers.py
@@ -1,15 +1,17 @@
-#!/usr/bin/env python2
+#!/usr/bin/python2
 # -*- coding: utf-8 -*-
 
 
 import sys
 import os
+import socket
 import xmlrpclib
+import argparse
 
 
 ##### Public methods #####
-def disableRetrackers(url) :
-	server = xmlrpclib.ServerProxy(sys.argv[1])
+def manageTrackers(client_url, enable_list, disable_list) :
+	server = xmlrpclib.ServerProxy(client_url)
 
 	multicall = xmlrpclib.MultiCall(server)
 	hashes_list = server.download_list()
@@ -17,21 +19,35 @@ def disableRetrackers(url) :
 		multicall.t.multicall(t_hash, "", "t.is_enabled=", "t.get_url=")
 	trackers_list = list(multicall())
 
+	actions_dict = dict.fromkeys(set(enable_list or ()), 1)
+	actions_dict.update(dict.fromkeys(set(disable_list or ()), 0))
+
 	multicall = xmlrpclib.MultiCall(server)
 	for count in xrange(len(hashes_list)) :
-		for (index, url) in trackers_list[count] :
-			if "retracker.local" in url :
-				multicall.t.set_enabled(hashes_list[count], index, 0)
+		for (index, (is_enabled, url)) in enumerate(trackers_list[count]) :
+			for (pattern, action) in actions_dict.iteritems() :
+				if pattern in url and action != is_enabled :
+					multicall.t.set_enabled(hashes_list[count], index, action)
+					print url, pattern, action
+					continue
 	multicall()
 
 
 ##### Main #####
 def main() :
-	if len(sys.argv) != 2 :
-		print >> sys.stderr, "Usage: %s <http://xmlrpchost/path>" % (os.path.basename(sys.argv[0]))
-		print >> sys.stderr, "Warning: rtorrent ONLY!"
-		sys.exit(1)
-	disableRetrackers(sys.argv[1])
+	cli_parser = argparse.ArgumentParser(description="Manage trackers (rtorrent only)")
+	cli_parser.add_argument(      "--enable",     dest="enable_list",  nargs="+", metavar="<pattern>")
+	cli_parser.add_argument(      "--disable",    dest="disable_list", nargs="+", metavar="<pattern>")
+	cli_parser.add_argument("-t", "--timeout",    dest="socket_timeout", action="store", default=5, type=int, metavar="<seconds>")
+	cli_parser.add_argument(      "--client-url", dest="client_url",     action="store", default="http://localhost/RPC2", metavar="<url>")
+
+	cli_options = cli_parser.parse_args(sys.argv[1:])
+	socket.setdefaulttimeout(cli_options.socket_timeout)
+	manageTrackers(
+		cli_options.client_url,
+		cli_options.enable_list,
+		cli_options.disable_list,
+	)
 
 
 ###
