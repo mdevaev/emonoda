1d1f89d3eb4d9f0b93a715cbd34baba97e2e18f5
Devaev Maxim <mdevaev@gmail.com>
2013-09-18 23:54:56 +0400

    rutracker: removed AJAX logic for fetchHash()

--- rtlib/fetchers/rutracker.py
+++ rtlib/fetchers/rutracker.py
@@ -26,19 +26,17 @@ from rtlib import tfile
 import urllib
 import urllib2
 import cookielib
-import json
 import re
 
 
 ##### Public constants #####
 FETCHER_NAME = "rutracker"
-FETCHER_VERSION = 0
+FETCHER_VERSION = 1
 
 RUTRACKER_DOMAIN = "rutracker.org"
 RUTRACKER_LOGIN_URL = "http://login.%s/forum/login.php" % (RUTRACKER_DOMAIN)
 RUTRACKER_VIEWTOPIC_URL = "http://%s/forum/viewtopic.php" % (RUTRACKER_DOMAIN)
 RUTRACKER_DL_URL = "http://dl.%s/forum/dl.php" % (RUTRACKER_DOMAIN)
-RUTRACKER_AJAX_URL = "http://%s/forum/ajax.php" % (RUTRACKER_DOMAIN)
 
 
 ##### Public classes #####
@@ -56,8 +54,7 @@ class Fetcher(fetcherlib.AbstractFetcher) :
 		self.__cap_sid_regexp = re.compile(r"name=\"cap_sid\" value=\"([a-zA-Z0-9]+)\"")
 		self.__cap_code_regexp = re.compile(r"name=\"(cap_code_[a-zA-Z0-9]+)\"")
 
-		self.__hash_t_regexp = re.compile(r"t_hash\s*:\s*'([a-zA-Z0-9]+)'")
-		self.__hash_form_token_regexp = re.compile(r"name=\"form_token\" value=\"([a-zA-Z0-9]+)\"")
+		self.__hash_regexp = re.compile(r"<span id=\"tor-hash\">([a-zA-Z0-9]+)</span>")
 
 		self.__cookie_jar = None
 		self.__opener = None
@@ -160,27 +157,9 @@ class Fetcher(fetcherlib.AbstractFetcher) :
 		assert not comment_match is None, "No comment match"
 
 		data = self.readUrlRetry(torrent.comment() or "")
-		hash_t_match = self.__hash_t_regexp.search(data)
-		hash_form_token_match = self.__hash_form_token_regexp.search(data)
-		self.assertFetcher(not hash_t_match is None, "Unknown t_hash")
-		self.assertFetcher(not hash_form_token_match is None, "Unknown form_token")
-
-		post_dict = {
-			"action" : "get_info_hash",
-			"topic_id" : comment_match.group(1),
-			"t_hash" : hash_t_match.group(1),
-			"form_token" : hash_form_token_match.group(1),
-		}
-		request = urllib2.Request(RUTRACKER_AJAX_URL, urllib.urlencode(post_dict), headers={
-				"User-Agent" : const.BROWSER_USER_AGENT,
-			})
-		response_dict = json.loads(self.readUrlRetry(request))
-		if response_dict.has_key("ih_hex") :
-			return response_dict["ih_hex"].lower()
-		elif response_dict.has_key("error_msg") : # Like self.assertFetcher()
-			raise fetcherlib.FetcherError(unicode(response_dict["error_msg"]).encode("utf-8"))
-		else :
-			raise fetcherlib.FetcherError("Invalid response: %s" % (str(response_dict)))
+		hash_match = self.__hash_regexp.search(data)
+		self.assertFetcher(not hash_match is None, "Hash not found")
+		return hash_match.group(1).lower()
 
 	def readUrlRetry(self, *args_list, **kwargs_dict) :
 		kwargs_dict.setdefault("opener", self.__opener)
