a111b78e948acc765185ef450cf2264365d08988
Devaev Maxim <mdevaev@gmail.com>
2013-10-10 14:53:27 +0400

    setup: refactoring

--- setup.py
+++ setup.py
@@ -2,36 +2,43 @@
 # -*- coding: utf-8 -*-
 
 
-from setuptools import setup
+from rtlib import const
+
+import setuptools
 
 
-setup(
-	name="rtfetch",
-	version="0.1",
-	url="https://github.com/mdevaev/rtfetch",
-	license="GPLv3",
-	author="Devaev Maxim",
-	author_email="mdevaev@gmail.com",
-	description="rtfetch -- The set of tools to organize and managament of your torrents",
-	platforms="any",
-	packages=[
-		"rtlib",
-		"rtlib/fetchers",
-		"rtlib/clients",
-	],
-	scripts=[
-		"rtfetch.py",
-		"rtquery.py",
-		"rtload.py",
-		"rtfile.py",
-		"rtdiff.py",
-		"rthook-manage-trackers.py",
-	],
-	classifiers=[
-		"Topic :: Software Development :: Libraries :: Python Modules",
-		"Development Status :: 3 - Alpha",
-		"Programming Language :: Python",
-		"Operating System :: OS Independent",
-	],
-)
+##### Main #####
+if __name__ == "__main__" :
+	setuptools.setup(
+		name="rtfetch",
+		version=const.VERSION,
+		url=const.UPSTREAM_URL,
+		license="GPLv3",
+		author="Devaev Maxim",
+		author_email="mdevaev@gmail.com",
+		description="The set of tools to organize and managament of your torrents",
+		platforms="any",
+
+		packages=(
+			"rtlib",
+			"rtlib/fetchers",
+			"rtlib/clients",
+		),
+
+		scripts=(
+			"rtfetch.py",
+			"rtquery.py",
+			"rtload.py",
+			"rtfile.py",
+			"rtdiff.py",
+			"rthook-manage-trackers.py",
+		),
+
+		classifiers=(
+			"Topic :: Software Development :: Libraries :: Python Modules",
+			"Development Status :: 3 - Alpha",
+			"Programming Language :: Python",
+			"Operating System :: OS Independent",
+		),
+	)
 
