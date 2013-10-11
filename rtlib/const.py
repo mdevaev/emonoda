5a9622dd0b252967fd0538bb3606910bad48034a
Devaev Maxim <mdevaev@gmail.com>
2013-10-11 13:17:21 +0400

    Version bump: 0.2

--- rtlib/const.py
+++ rtlib/const.py
@@ -20,7 +20,7 @@
 
 
 ##### Public constants #####
-VERSION_TUPLE = (0, 1)
+VERSION_TUPLE = (0, 2)
 VERSION = ".".join(map(str, VERSION_TUPLE))
 
 UPSTREAM_URL = "https://github.com/mdevaev/rtfetch"
