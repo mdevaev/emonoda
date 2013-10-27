dd33c520c2e2cd8837a9752f7730e28d3723c6ec
Devaev Maxim <mdevaev@gmail.com>
2013-10-28 01:06:59 +0400

    ktorrent: fixed copyright

--- rtlib/clients/ktorrent.py
+++ rtlib/clients/ktorrent.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 #
-#    transmission client for rtfetch
-#    Copyright (C) 2013  Vitaly Lipatov <lav@etersoft.ru>, Devaev Maxim <mdevaev@gmail.com>
+#    KTorrent client for rtfetch
+#    Copyright (C) 2013  Devaev Maxim <mdevaev@gmail.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
