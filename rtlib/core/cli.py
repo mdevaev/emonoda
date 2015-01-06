7d1b6c95b51bdeacb92f49474116a0e19b3658d5
Devaev Maxim <mdevaev@gmail.com>
2015-01-06 23:03:50 +0300

    Own _inner_print()

--- rtlib/core/cli.py
+++ rtlib/core/cli.py
@@ -1,23 +1,23 @@
 import sys
 
-from ulib.ui import cli
-
 from colorama import Fore
 from colorama import Style
 
 
 # =====
-class Log:
-    _colors = {
-        "red":     Style.BRIGHT + Fore.RED,
-        "green":   Style.BRIGHT + Fore.GREEN,
-        "yellow":  Style.BRIGHT + Fore.YELLOW,
-        "cyan":    Style.BRIGHT + Fore.CYAN,
-        "magenta": Style.BRIGHT + Fore.MAGENTA,
-        "blue":    Style.BRIGHT + Fore.BLUE,
-        "reset":   Fore.RESET,
-    }
+_COLORS = {
+    "red":     Style.BRIGHT + Fore.RED,
+    "green":   Style.BRIGHT + Fore.GREEN,
+    "yellow":  Style.BRIGHT + Fore.YELLOW,
+    "cyan":    Style.BRIGHT + Fore.CYAN,
+    "magenta": Style.BRIGHT + Fore.MAGENTA,
+    "blue":    Style.BRIGHT + Fore.BLUE,
+    "reset":   Fore.RESET,
+}
 
+
+# =====
+class Log:
     def __init__(self, use_colors=True, force_colors=False, output=sys.stdout):
         self._use_colors = use_colors
         self._force_colors = force_colors
@@ -29,13 +29,31 @@ class Log:
         output = self._select(output, self._output)
 
         if use_colors and (output.isatty() or force_colors):
-            colors = self._colors
+            colors = _COLORS
         else:
-            colors = dict.fromkeys(list(self._colors), "")
+            colors = dict.fromkeys(list(_COLORS), "")
 
         text = text.format(**colors)
-        handler = (cli.one_line if one_line else cli.new_line)
-        handler(text, output=output)
+        _inner_print(text, one_line, output)
 
     def _select(self, first, second):
         return (first if first is not None else second)
+
+
+# =====
+_last_text = ""
+
+
+def _inner_print(text, one_line, output):
+    global _last_text  # pylint: disable=global-statement
+    if one_line:
+        to_print = " " * len(_last_text) + "\r" + text + "\r"
+        _last_text = text
+    else:
+        if len(_last_text) != 0:
+            to_print = " " * len(_last_text) + "\r" + text + "\n"
+        else:
+            to_print = text + "\n"
+        _last_text = ""
+    output.write(to_print)
+    output.flush()
