d25c06f336a663789cdc532f75b7a4a73e2d723f
Devaev Maxim <mdevaev@gmail.com>
2015-01-07 13:27:36 +0300

    Removed deps on contextlog

--- rtlib/optconf/loaders/yaml.py
+++ rtlib/optconf/loaders/yaml.py
@@ -2,13 +2,10 @@ import os
 import yaml
 import yaml.loader
 
-from contextlog import get_logger
-
 
 # =====
 def load_file(file_path):
     with open(file_path) as yaml_file:
-        get_logger().debug("Loading config from '%s'...", file_path)
         try:
             return yaml.load(yaml_file, _YamlLoader)
         except Exception:
@@ -25,6 +22,5 @@ class _YamlLoader(yaml.loader.Loader):  # pylint: disable=too-many-ancestors
     def include(self, node):
         # Logger which supports include-files
         file_path = os.path.join(self._root, self.construct_scalar(node))  # pylint: disable=no-member
-        get_logger().debug("Including config '%s'...", file_path)
         return load_file(file_path)
 _YamlLoader.add_constructor("!include", _YamlLoader.include)  # pylint: disable=no-member
