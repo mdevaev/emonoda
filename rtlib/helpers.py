44269b8c4e4d97456175976f64d0ec39c8b840aa
Devaev Maxim <mdevaev@gmail.com>
2015-01-13 22:28:06 +0300

    style fix

--- rtlib/helpers.py
+++ rtlib/helpers.py
@@ -4,12 +4,16 @@ from . import fmt
 
 # =====
 def load_torrents_from_dir(dir_path, name_filter, log):
-    fan = fmt.make_fan()
+    if log.isatty():
+        fan = fmt.make_fan()
 
-    def load_torrent(path):
-        log.print("# Caching {cyan}%s/{yellow}%s {magenta}%s{reset}" % (
-                  dir_path, name_filter, next(fan)), one_line=True)
-        return tfile.load_torrent_from_path(path)
+        def load_torrent(path):
+            log.print("# Caching {cyan}%s/{yellow}%s {magenta}%s{reset}" % (
+                      dir_path, name_filter, next(fan)), one_line=True)
+            return tfile.load_torrent_from_path(path)
+    else:
+        log.print("# Caching {cyan}%s/{yellow}%s ..." % (dir_path, name_filter))
+        load_torrent = tfile.load_torrent_from_path
 
     torrents = tfile.load_from_dir(dir_path, name_filter, as_abs=True, loader=load_torrent)
 
