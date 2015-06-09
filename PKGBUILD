fa85e92ecc75b6d11ace393fab50d3bc7c618d8a
Devaev Maxim <mdevaev@gmail.com>
2015-06-09 15:19:35 +0300

    Bump version: 1.9.12 → 1.9.13

--- PKGBUILD
+++ PKGBUILD
@@ -3,7 +3,7 @@
 
 
 pkgname="emonoda"
-pkgver="1.9.12"
+pkgver="1.9.13"
 pkgrel="1"
 pkgdesc="A set of tools to organize and manage your torrents"
 arch=("any")
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ import setuptools
 if __name__ == "__main__":
     setuptools.setup(
         name="emonoda",
-        version="1.9.12",
+        version="1.9.13",
         url="https://github.com/mdevaev/emonoda",
         license="GPLv3",
         author="Devaev Maxim",
