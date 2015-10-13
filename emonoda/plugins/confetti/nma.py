bcb1b896c9e74e4b437d7c0b3537408e8f0a79f9
Devaev Maxim <mdevaev@gmail.com>
2015-10-13 16:54:06 +0300

    NMA confetti

---
+++ emonoda/plugins/confetti/nma.py
@@ -0,0 +1,91 @@
+"""
+    Emonoda -- A set of tools to organize and manage your torrents
+    Copyright (C) 2015  Devaev Maxim <mdevaev@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+import urllib.parse
+
+from ...optconf import Option
+from ...optconf.converters import as_string_list
+
+from ... import web
+
+from . import BaseConfetti
+from . import ST
+
+
+# =====
+class Plugin(BaseConfetti):
+    def __init__(self, api_keys, **kwargs):  # pylint: disable=super-init-not-called
+        self._init_bases(**kwargs)
+
+        self._api_keys = api_keys
+
+    @classmethod
+    def get_name(cls):
+        return "nma"
+
+    @classmethod
+    def get_options(cls):
+        return cls._get_merged_options({
+            "api_keys": Option(default=["CHANGE_ME"], type=as_string_list, help="API keys (or one key as string)"),
+        })
+
+    # ===
+
+    def send_results(self, app, results):
+        for attrs in results[ST.UPDATED].values():
+            self._notify(
+                app="Emonoda ({})".format(app),
+                event="{}".format(attrs["torrent"].get_name()),
+                description=self._format_description(attrs["diff"]),
+            )
+
+    # ===
+
+    def _format_description(self, diff):
+        description_lines = []
+        for (sign, items) in (
+            ("+", diff.added),
+            ("-", diff.removed),
+            ("~", diff.modified),
+            ("?", diff.type_modified),
+        ):
+            for item in sorted(items):
+                description_lines.append("[{}] {}".format(sign, item))
+        description = "Affected {} files\n".format(len(description_lines)) + "\n".join(description_lines)
+        if len(description) > 9999:
+            description = description[:9995] + "..."
+        return description
+
+    def _notify(self, app, event, description):
+        # http://www.notifymyandroid.com/api.jsp
+        post = {
+            "apikey":       ",".join(self._api_keys),
+            "application":  app,
+            "event":        event,
+            "description":  description,
+            "Content-Type": "text/plain",
+        }
+        web.read_url(
+            opener=web.build_opener(),  # TODO: Add proxy
+            url="https://www.notifymyandroid.com/publicapi/notify",
+            data=urllib.parse.urlencode(post).encode("utf-8"),
+            timeout=self._timeout,
+            retries=self._retries,
+            retries_sleep=self._retries_sleep,
+        )
