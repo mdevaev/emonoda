274e46d7230c86832ffbf1be5f63b2258c9e0f70
Devaev Maxim <mdevaev@gmail.com>
2016-01-01 03:41:25 +0300

    nnm-club: fixed scrape url

--- emonoda/plugins/trackers/nnm_club_me.py
+++ emonoda/plugins/trackers/nnm_club_me.py
@@ -38,7 +38,7 @@ class Plugin(BaseTracker, WithLogin, WithSimplePostLogin, WithScrape, WithDownlo
 
     _COMMENT_REGEXP = re.compile(r"http://nnm-club\.(me|ru)/forum/viewtopic\.php\?p=(\d+)")
 
-    _SCRAPE_URL = "http://bt.{}:2710/scrape.php?info_hash={{scrape_hash}}".format(_NNM_DOMAIN)
+    _TORRENT_SCRAPE_URL = "http://bt.{}:2710/scrape.php?info_hash={{scrape_hash}}".format(_NNM_DOMAIN)
 
     # ===
 
