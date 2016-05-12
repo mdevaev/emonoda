be0ba959caa95cb2c8d5afa65b6a9ebd5ad85e25
ASM <cpp.create@gmail.com>
2016-05-12 04:48:08 +0300

    Fixed tests

--- emonoda/apps/emconfetti_test.py
+++ emonoda/apps/emconfetti_test.py
@@ -13,84 +13,65 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-
 import sys
-import os
-import json
-import shutil
-import contextlib
-import traceback
-import threading
-import operator
-import argparse
 
-from ..plugins.trackers import TrackerError
-from ..plugins.trackers import WithCheckHash
-from ..plugins.trackers import WithCheckScrape
-from ..plugins.trackers import WithCheckTime
-
-from ..helpers import tcollection
 from ..helpers import surprise
-
-from .. import tfile
-from .. import fmt
-from .. import tools
 
 from . import init
 from . import get_configured_log
-from . import get_configured_client
-from . import get_configured_trackers
 from . import get_configured_confetti
+
 
 class Torrent:
     def get_comment(self):
         return "Test, test, test"
 
+
 class Tracker:
     PLUGIN_NAME = "TEST.org.ru"
 
+
 def main():
     (parent_parser, argv, config) = init()
-    with get_configured_log(config, False, sys.stdout) as log_stdout:
-        with get_configured_log(config, False, sys.stderr) as log_stderr:
-            results = {
-                "affected":{
-                    "test.torrent":{
-                        "diff":{
-                            "added": ["nya.mkv", "nya.srt"],
-                            "removed": ["nyaa.srt", "nyaa.mkv"],
-                            "modified": [],
-                            "type_modified": ["list.lst"]
-                        },
-                        "torrent": Torrent(), "tracker":Tracker()
+    with get_configured_log(config, False, sys.stderr) as log_stderr:
+        results = {
+            "affected": {
+                "test.torrent": {
+                    "diff": {
+                        "added": ["nya.mkv", "nya.srt"],
+                        "removed": ["nyaa.srt", "nyaa.mkv"],
+                        "modified": [],
+                        "type_modified": ["list.lst"]
                     },
-                    "test1.torrent":{
-                        "diff":{
-                            "added": ["nya.mkv", "nya.srt"],
-                            "removed": ["nyaa.srt", "nyaa.mkv"],
-                            "modified": [],
-                            "type_modified": ["list.lst"]
-                        },
-                        "torrent": Torrent(), "tracker":Tracker()
-                    }
+                    "torrent": Torrent(), "tracker": Tracker()
                 },
-                "passed":{},
-                "not_in_client":{},
-                "unknown":{},
-                "invalid":{},
-                "tracker_error": {},
-                "unhandled_error": {}
-            }
-            confetti = get_configured_confetti(
-                config=config,
-                log=log_stderr,
-            )
-            surprise.deploy_surprise(
-                    source="emupdate",
-                    results=results,
-                    confetti=confetti,
-                    log=log_stderr,
-            )
+                "test1.torrent": {
+                    "diff": {
+                        "added": ["nya.mkv", "nya.srt"],
+                        "removed": ["nyaa.srt", "nyaa.mkv"],
+                        "modified": [],
+                        "type_modified": ["list.lst"]
+                    },
+                    "torrent": Torrent(), "tracker": Tracker()
+                }
+            },
+            "passed": {},
+            "not_in_client": {},
+            "unknown": {},
+            "invalid": {},
+            "tracker_error": {},
+            "unhandled_error": {}
+        }
+        confetti = get_configured_confetti(
+            config=config,
+            log=log_stderr,
+        )
+        surprise.deploy_surprise(
+            source="emupdate",
+            results=results,
+            confetti=confetti,
+            log=log_stderr,
+        )
 
 if __name__ == "__main__":
     main()
