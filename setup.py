7a57b1af6d916d8e3a47308513ab75ae3cbf45e4
Devaev Maxim <mdevaev@gmail.com>
2016-05-25 09:13:51 +0300

    Bump version: 2.0.19 → 2.0.20

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ import setuptools
 if __name__ == "__main__":
     setuptools.setup(
         name="emonoda",
-        version="2.0.19",
+        version="2.0.20",
         url="https://github.com/mdevaev/emonoda",
         license="GPLv3",
         author="Devaev Maxim",
