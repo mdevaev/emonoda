d330ad3c441f7ffa935709642757418d46556696
Devaev Maxim <mdevaev@gmail.com>
2018-06-21 04:39:33 +0300

    Bump version: 2.1.15 → 2.1.16

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from Cython.Build import cythonize
 if __name__ == "__main__":
     setup(
         name="emonoda",
-        version="2.1.15",
+        version="2.1.16",
         url="https://github.com/mdevaev/emonoda",
         license="GPLv3",
         author="Devaev Maxim",
