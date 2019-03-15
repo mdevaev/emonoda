0a609b9f4ff843b1863dbb54b11a33ce2cf7e0c1
Devaev Maxim <mdevaev@gmail.com>
2019-03-15 11:44:10 +0300

    Bump version: 2.1.24 → 2.1.25

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def main() -> None:
 
     setup(
         name="emonoda",
-        version="2.1.24",
+        version="2.1.25",
         url="https://github.com/mdevaev/emonoda",
         license="GPLv3",
         author="Devaev Maxim",
